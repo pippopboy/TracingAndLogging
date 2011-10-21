e8f6997c72790fa82952dad6abcc5049cf85b736
Trong Nghia <trongnghia206@gmail.com>
2011-10-21 11:17:53 +0700

    tracinggit

---
+++ TracingLoggingAuthorsService/Authors.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Threading;
+
+namespace TracingLoggingAuthorsService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class Authors : IAuthors
+    {
+        public Author Getauthors()
+        {
+            Thread.Sleep(10000);
+            Author au = new Author();
+            au.ProjectName = "Chat Application";
+            au.GroupName = "Cao Tran Trong Nghia";
+            return au;
+        }
+    }
+}
---
+++ TracingLoggingAuthorsService/IAuthors.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace TracingLoggingAuthorsService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IAuthors
+    {
+        [OperationContract]
+        Author Getauthors();
+    }
+
+    [DataContract]
+    public class Author
+    {
+        [DataMember]
+        public string ProjectName;
+
+        [DataMember]
+        public string GroupName;
+    } 
+    // Use a data contract as illustrated in the sample below to add composite types to service operations
+   
+}
